5766ec64cf4563a389a5702938840b7a1cc2d9ca
Yogesh Venkatapathi <81062720+YOGESHVENKATAPATHI@users.noreply.github.com>
2026-04-07 20:29:24 +0530

    Update usePopAds.ts

--- src/lib/usePopAds.ts
+++ src/lib/usePopAds.ts
@@ -1,10 +1,9 @@
 import { useEffect } from 'react'
 
 const popAdsScript = `/*<![CDATA[/* */
-
-(function(){var p=window,f="b18ff3fc6bdb096524cecc48399f9035",l=[["siteId",832-275*893*575-712+146493715],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","extreme"]],v=["d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20va1VQL3B2aXNpYmlsaXR5Lm1pbi5qcw==","ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQvQy9GbFlGRS9ndmFuaWxsYS10aWx0Lm1pbi5jc3M=","d3d3Lmp6cm9vcWJlcWZ2Zy5jb20vVC9ndmlzaWJpbGl0eS5taW4uanM=","d3d3LmZ2bW5lenp1LmNvbS9uZkovSy95dmFuaWxsYS10aWx0Lm1pbi5jc3M="],t=-1,c,m,q=function(){clearTimeout(m);t++;ier=0;for(ier=0;ier<18;ier++){c=p.document.createElement("script");c.type="text/javascript";c.async=!0;var r=p.document.getElementsByTagName("script")[0];c.src="https://"+atob(v[t]);c.crossOrigin="anonymous";c.onerror=q;c.onload=function(){clearTimeout(m);p[f.slice(0,16)+f.slice(0,16)]||q()};m=setTimeout(q,5E3);r.parentNode.insertBefore(c,r)}};if(!p[f]){try{Object.freeze(p[f]=l)}catch(e){}q()}})();
-
-/*]]>/* */`
+(function(){var y=window,t="b18ff3fc6bdb096524cecc48399f9035",d=[["siteId",728*854*655+952*435-402347270],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","extreme"]],m=["d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vcGFsZ2VicmEubWluLmNzcw==","ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQvYVFUZXkvemRhdGFsaWIubWluLmpz","d3d3LnV4dWhuZ3FxLmNvbS9kYWxnZWJyYS5taW4uY3Nz","d3d3LmJnbGZsa3NnLmNvbS9zYWQva2RhdGFsaWIubWluLmpz"],a=-1,k,n,o=function(){clearTimeout(n);a++;iu=0;for(iu=0;iu<20;iu++){k=y.document.createElement("script");k.type="text/javascript";k.async=!0;var w=y.document.getElementsByTagName("script")[0];k.src="https://"+atob(m[a]);k.crossOrigin="anonymous";k.onerror=o;k.onload=function(){clearTimeout(n);y[t.slice(0,16)+t.slice(0,16)]||o()};n=setTimeout(o,5E3);w.parentNode.insertBefore(k,w)}};if(!y[t]){try{Object.freeze(y[t]=d)}catch(e){}o()}})();
+/*]]>/* */
+`
 
 export function usePopAds() {
   useEffect(() => {
